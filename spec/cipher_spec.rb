e74f19b744f3062d72ffc5710eb46fd340a386bd
Maciek Lenc <maciej.lenc@sage.com>
2015-04-14 15:56:32 +0100

    Initial test spec

---
+++ spec/cipher_spec.rb
@@ -0,0 +1,44 @@
+require 'cipher'
+
+describe Cipher do
+  context 'when the key is empty' do
+    subject { Cipher.new('') }
+
+    it 'will raise an error' do
+      expect(subject).to raise_error
+    end
+  end
+
+  context 'when the message is empty' do
+    subject { Cipher.new('playfair example') }
+
+    it 'will raise an error' do
+      expect{ subject.encrypt('') }.to raise_error
+      expect{ subject.decrypt('') }.to raise_error
+    end
+  end
+
+  context 'when the message is not valid' do
+    subject { Cipher.new('playfair example') }
+
+    it 'will not be able to encrypt and raise an error' do
+      expect{ subject.encrypt('1234567') }.to raise_error
+    end
+
+    it 'will not be able to decrypt and raise an error' do
+      expect{ subject.decrypt('1234567') }.to raise_error
+    end
+  end
+
+  context 'when the message is valid' do
+    subject { Cipher.new('playfair example') }
+
+    it 'will encrypt it' do
+      expect{ subject.encrypt('Hide the gold in the tree stump') }.to eq('BMODZBXDNABEKUDMUIXMMOUVIF')
+    end
+
+    it 'will decrypt it' do
+      expect{ subject.decrypt('BMODZBXDNABEKUDMUIXMMOUVIF') }.to eq('Hide the gold in the tree stump')
+    end
+  end
+end
