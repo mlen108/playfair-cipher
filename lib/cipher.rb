e37abe38c33246bdd7a7b591a977210181b00a19
Maciek Lenc <maciej.lenc@sage.com>
2015-04-17 10:41:33 +0100

    Encrypt/decrypt the Cipher message.
    Now, refactor the mad bits and bring the code to some standards using code metrics.

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,57 +1,152 @@
 class Cipher
-  attr_reader :key, :grid
+  attr_reader :grid
 
-  def initialize(key)
-    @key = prepare(key)
+  def initialize(key, message)
+    key = prepare(key)
+    message = prepare(message)
 
-    check(@key)
+    check(key)
+    check(message)
 
-    @grid = generate_grid
+    @digraphs = Message.new(key, message)
   end
 
-  def encrypt(msg)
-    check(msg)
+  def encrypt
+    @digraphs.map { |d| d.encrypt }.join
   end
 
-  def decrypt(msg)
-    check(msg)
+  def decrypt
+    @digraphs.map { |d| d.decrypt }.join
   end
 
   private
-    def prepare(str)
-      str.delete(' ').upcase
+
+  def prepare(str)
+    str.delete(' ').upcase
+  end
+
+  def check(str)
+    if str.nil? || str.length == 0
+      fail 'It cannot be empty.'
     end
 
-    def check(str)
-      if str.nil? || str.length == 0
-        fail 'It cannot be empty.'
-      end
+    fail 'It must be in range of A-Z characters.' unless num?(str)
+  end
 
-      if num?(str)
-        fail 'It must be in range of A-Z characters.'
+  def num?(str)
+    !str.match(/[^[:alpha:]]/)
+  end
+end
+
+class Message
+  include Enumerable
+
+  attr_reader :key, :message
+
+  def initialize(key, message)
+    @key = key
+    @msg = message
+
+    @grid = ''
+    memo_key
+  end
+
+  def memo_key
+    grid = ''
+    alphabet = ('A'..'Z').to_a
+
+    @key.chars.each do |l|
+      if !grid.include?(l) && alphabet.include?(l)
+        grid += l
       end
     end
 
-    def num?(str)
-      str.match(/[^[:alpha:]]/)
+    alphabet.each do |l|
+      if l == 'J'
+        l = 'I'
+      end
+      if !grid.include?(l)
+        grid += l
+      end
     end
 
-    def generate_grid
-      grid = ''
-      alphabet = 'ABCDEFGHIJKLMNOPRSTUVWXYZ'
+    @grid = grid
+  end
 
-      @key.chars.each do |l|
-        if !grid.include?(l) && alphabet.include?(l)
-          grid += l
-        end
+  def each
+    msg_fixed = @msg.tr('J', 'I')
+
+    counter = 0
+    while counter < msg_fixed.size
+      if counter + 1 == msg_fixed.size
+        yield Digraph.new(msg_fixed[counter], 'X', @grid)
+        break
+      elsif msg_fixed[counter] != msg_fixed[counter + 1]
+        yield Digraph.new(msg_fixed[counter], msg_fixed[counter + 1], @grid)
+        counter += 2
+      else
+        yield Digraph.new(msg_fixed[counter], 'X', @grid)
+        counter += 1
       end
-
-      alphabet.chars.each do |l|
-        if !grid.include?(l)
-          grid += l
-        end
-      end
-
-      grid
     end
+  end
+end
+
+class Digraph
+  # TODO(mlen) - refactor this encrypt/decrypt madness
+  attr_reader :first_char, :second_char, :grid
+
+  def initialize(first_char, second_char, grid)
+    @first_char = first_char
+    @second_char = second_char
+    @grid = grid
+  end
+
+  def encrypt
+    first_char_pos = grid.index(first_char)
+    second_char_pos = grid.index(second_char)
+
+    first_char_coords = [first_char_pos % 5, first_char_pos / 5]
+    second_char_coords = [second_char_pos % 5, second_char_pos / 5]
+
+    # letters appear on the same row
+    if first_char_coords[1] == second_char_coords[1]
+      first_encrypted = grid[(first_char_coords[1] * 5) + ((first_char_coords[0] + 1) % 5)]
+      second_encrypted = grid[(second_char_coords[1] * 5) + ((second_char_coords[0] + 1) % 5)]
+    # letters appear on the same column
+    elsif first_char_coords[0] == second_char_coords[0]
+      first_encrypted = grid[(((first_char_coords[1] + 1) % 5) * 5) + first_char_coords[0]]
+      second_encrypted = grid[(((second_char_coords[1] + 1) % 5) * 5) + second_char_coords[0]]
+    # letters are not on the same row or column (they form a rectangle)
+    else
+      first_encrypted = grid[(first_char_coords[1] * 5) + second_char_coords[0]]
+      second_encrypted = grid[(second_char_coords[1] * 5) + first_char_coords[0]]
+    end
+
+    first_encrypted + second_encrypted
+  end
+
+  def decrypt
+    first_char_pos = grid.index(first_char)
+    second_char_pos = grid.index(second_char)
+
+    first_char_coords = [first_char_pos % 5, first_char_pos / 5]
+    second_char_coords = [second_char_pos % 5, second_char_pos / 5]
+
+    # letters appear on the same row
+    if first_char_coords[1] == second_char_coords[1]
+      first_decrypted = grid[(first_char_coords[1] * 5) + ((first_char_coords[0] - 1) % 5)]
+      second_decrypted = grid[(second_char_coords[1] * 5) + ((second_char_coords[0] - 1) % 5)]
+    # letters appear on the same column
+    elsif first_char_coords[0] == second_char_coords[0]
+      first_decrypted = grid[(((first_char_coords[1] - 1) % 5) * 5) + first_char_coords[0]]
+      second_decrypted = grid[(((second_char_coords[1] - 1) % 5) * 5) + second_char_coords[0]]
+    # letters are not on the same row or column (they form a rectangle)
+    else
+      first_decrypted = grid[(first_char_coords[1] * 5) + second_char_coords[0]]
+      second_decrypted = grid[(second_char_coords[1] * 5) + first_char_coords[0]]
+    end
+
+    first_decrypted + second_decrypted
+  end
 end
--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -1,67 +1,53 @@
 require 'cipher'
 
 describe Cipher do
-  context 'when the key is empty' do
+  context 'when the key and message are empty' do
     it 'will raise an error' do
       expect{ described_class.new }.to raise_error
       expect{ described_class.new('') }.to raise_error
+      expect{ described_class.new('', '') }.to raise_error
     end
   end
 
   context 'when the key is not valid' do
     it 'will raise an error' do
-      expect{ described_class.new('?#(&$') }.to raise_error
-      expect{ described_class.new('1234567') }.to raise_error
+      expect{ described_class.new('?#(&$', 'abc') }.to raise_error
+      expect{ described_class.new('1234567', 'abc') }.to raise_error
     end
   end
 
   context 'when the key is valid' do
-    subject{ described_class.new('playfair example') }
-
     it 'will not raise an exception' do
-      expect{ described_class.new('abc') }.not_to raise_error
-      expect{ described_class.new('ABC') }.not_to raise_error
-    end
-
-    it 'will generate the correct key table' do
-      expect(subject.grid).to eq('PLAYFIREXMBCDGHJKNOSTUVWZ')
-    end
-
-    it 'will make the key table containing 25 chars in total' do
-      expect(subject.grid.length).to eq(25)
+      expect{ described_class.new('playfair example', 'abc') }.not_to raise_error
+      expect{ described_class.new('playfair example', 'ABC') }.not_to raise_error
     end
   end
 
   context 'when the message is empty' do
-    subject{ described_class.new('playfair example') }
-
     it 'will raise an error' do
-      expect{ subject.encrypt('') }.to raise_error
-      expect{ subject.decrypt('') }.to raise_error
+      expect{ described_class.new('playfair example', '') }.to raise_error
     end
   end
 
   context 'when the message is not valid' do
-    subject{ described_class.new('playfair example') }
-
-    it 'will not encrypt it & raise an error' do
-      expect{ subject.encrypt('1234567') }.to raise_error
-    end
-
-    it 'will not decrypt it & raise an error' do
-      expect{ subject.decrypt('1234567') }.to raise_error
+    it 'will raise an error' do
+      expect{ described_class.new('playfair example', '1234567') }.to raise_error
     end
   end
 
   context 'when the message is valid' do
-    subject{ described_class.new('playfair example') }
+    subject{ described_class.new('playfair example', 'Hide the gold in the tree stump') }
 
     it 'will encrypt it' do
-      expect{ subject.encrypt('Hide the gold in the tree stump') }.to eq('BMODZBXDNABEKUDMUIXMMOUVIF')
+      expect(subject.encrypt).to eq('BMODZBXDNABEKUDMUIXMMOUVIF')
     end
+  end
+
+  context 'when the message is valid' do
+    subject{ described_class.new('playfair example', 'BMODZ BXDNA BEKUD MUIXM MOUVI F') }
 
     it 'will decrypt it' do
-      expect{ subject.decrypt('BMODZBXDNABEKUDMUIXMMOUVIF') }.to eq('Hide the gold in the tree stump')
+      expect(subject.decrypt).to eq('HIDETHEGOLDINTHETREXESTUMP')
     end
   end
 end
